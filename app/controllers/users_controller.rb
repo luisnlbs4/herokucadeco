ab25720e19811b036bd2d9c0d84a40d3b5d0b8c6
Luis Veizaga <luisnlbs4@gmail.com>
2019-04-29 01:34:14 -0400

    asd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,11 +36,11 @@ class UsersController < ApplicationController
       @llamada.idEmisor = current_user.id
       @llamada.idReceptor = params[:idemisor]
       @llamada.estado = "espera"
+      @llamada.idSala = (@sala.id).to_s
+      @llamada.save
       @sala  = @salas.order("minutos DESC").first
       @sala.estado = "ocupado"
       @sala.save
-      @llamada.idSala = (@sala.id).to_s
-      @llamada.save
       redirect_to "/llamadaslibres/"+(@llamada.id).to_s and return
     else
       redirect_to "/" and return
